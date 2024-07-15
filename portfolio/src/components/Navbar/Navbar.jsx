bc65a013dab37d3f0db1c808d8bfb97a11033d4e
U Harshitha <122729066+U-Harshitha@users.noreply.github.com>
2024-07-15 21:24:26 +0530

    New project

---
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <>
+    <nav className='nav-wrapper'>
+        <div className='nav-content'>
+            <img className='logo' src='./assets'
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
